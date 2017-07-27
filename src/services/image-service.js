fa6fe2a5013c0d0275050110dd9b3298c09d2662
Tom Pennington <tompennington.92@gmail.com>
2017-07-27 10:57:17 +0100

    Add npm scripts to build out CSS from SASS.

--- src/services/image-service.js
+++ src/services/image-service.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
-const url = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?&tags=surfing';
+const url = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?&tags=efl%20championship';
 
 export default class ImageService {
 
