af12cc6c795cfd839dd9a25c84bc8cbad4af8d01
Tom Pennington <tompennington.92@gmail.com>
2017-07-27 22:29:45 +0100

    Display liked images.

--- src/js/components/likes.js
+++ src/js/components/likes.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import LocalStorageService from '../services/local-storage-service';
+import Card from './card';
 
 class Likes extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      likes: []
+    }
+
+    this.localStorageService = new LocalStorageService();
+  }
+
+  componentDidMount() {
+    let currentLikes = this.localStorageService.get('likes');
+    this.setState({ likes: currentLikes });
+  }
+
   render() {
     return (
       <div>
-        <h2>Likes</h2>
+        <div className="container">
+          <h1>Photos you've liked</h1>
+          {this.state.likes && this.state.likes.map((like, index) =>
+            <Card key={index}
+                  title={like.title}
+                  imageSrc={like.imageSrc} />
+          )}
+
+          {!this.state.likes && 
+            <p>You haven't liked any photos yet.</p>
+          }
+        </div>
       </div>
     );
   }
